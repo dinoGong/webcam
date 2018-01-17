d77b2a6a1ad8cf9d1a3f28dc99bd7d16c681602a
gpiopi <gpiopi@sina.com>
2018-01-17 21:37:34 +0800

    up

---
+++ server.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask, request, redirect, url_for,render_template
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html',title="hello")
+if __name__=='__main__':
+    app.run(debug=True,host='0.0.0.0',port=5000)
+
